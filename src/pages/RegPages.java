dc16fd48324dd79fb169940fb4ef39e90d2a4537
sachin <navalesachin46@gmail.com>
2022-03-30 10:15:39 +0530

    Added new files

---
+++ src/pages/RegPages.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class RegPages {
+
+	public static void main(String[] args) 
+	{
+System.out.println("This is my registration page");
+		
+	}
+
+}
---
+++ src/tests/MyTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class MyTest
+{
+
+	public static void main(String[] args)
+	{
+		String s="sunil shetty";
+		System.out.println(s);
+
+	}
+
+}
