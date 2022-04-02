02c53915c3c028cfd964d699362290564ec9c3da
sachin <navalesachin46@gmail.com>
2022-04-02 09:52:15 +0530

    Feature 2

--- src/pages/FeatureOne.java
+++ src/pages/FeatureOne.java
@@ -5,6 +5,7 @@ public class FeatureOne
     public static void main(String[] args)
     {
         System.out.println("This is my feature 1");
+        System.out.println("This is my feature 1");
     }
 
 
